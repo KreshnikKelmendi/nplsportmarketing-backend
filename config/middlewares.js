6cc3afb860713d9105125f1df7fb965c030ecd92
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-11-24 18:58:14 -1200

    ddsds

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,35 +1,9 @@
-// module.exports = [
-//   'strapi::errors',
-//   {
-//     name: 'strapi::security',
-//     config: {
-//       contentSecurityPolicy: {
-//         useDefaults: true,
-//         directives: {
-//           'connect-src': ["'self'", 'https:'],
-//           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-//           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-//           upgradeInsecureRequests: null,
-//         },
-//       },
-//     },
-//   },
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::logger',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-
 module.exports = {
-  //...
   settings: {
     cors: {
-      origin: ['*'], //allow all origins
-      headers: ['*'], //allow all headers
+      enabled: true,
+      headers: '*',
+      methods: '*',
     },
   },
 };
