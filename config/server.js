ab322941de852b6111e6b6a9c10d09677e8f9a8d
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-12-05 13:26:27 -1200

    ca

--- config/server.js
+++ config/server.js
@@ -7,4 +7,11 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  cache: {
+    enabled: true,
+    models: ['lajmet', 'shpalljet'], // List the models you want to cache
+    stores: ['memory'], // Use in-memory cache
+    max: 100, // Max number of items in the cache
+    ttl: 60 * 10, // Cache TTL in seconds (e.g., 10 minutes)
+  },
 });
